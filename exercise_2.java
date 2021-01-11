a6eed695e34c900277a3a69b1a4241a73d5da1bf
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-01-11 19:48:33 +0200

    Update exercise_2.java

--- exercise_2.java
+++ exercise_2.java
@@ -12,6 +12,7 @@ public class Main {
 
         print_a_pow_b_and_c_pow_d(2,8, 2,10);
         myCalc("*", 5, 2);
+        myCalc(5, 2);
 
         int arr[] = new int[5];
         create_arr_random(r, arr);
@@ -36,6 +37,28 @@ public class Main {
     }
 
     private static void myCalc(String s, int a, int b){
+        switch (s){
+            case ("+"):
+                System.out.println(String.format("%d + %d = %d", a, b, a + b ));
+                break;
+            case ("-"):
+                System.out.println(String.format("%d - %d = %d", a, b, a - b ));
+                break;
+            case ("*"):
+                System.out.println(String.format("%d * %d = %d", a, b, a * b ));
+                break;
+            case ("div"):
+                if (b != 0){
+                    System.out.println(String.format("%d / %d = %f", a, b, (float)a / b ));
+                } else {
+                    System.out.println("Error! Division by zero");
+                }
+                break;
+        }
+    }
+
+    private static void myCalc(int a, int b){
+        String s = "+";
         switch (s){
             case ("+"):
                 System.out.println(String.format("%d + %d = %d", a, b, a + b ));
